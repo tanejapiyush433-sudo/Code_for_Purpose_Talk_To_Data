db3d742e0dec08248405b6b9ce0ec820a2c3ec82
tanejapiyush433-sudo <tanejapiyush433@gmail.com>
2026-04-10 23:55:01 +0530

    Update anomaly.py

--- src/anomaly.py
+++ src/anomaly.py
@@ -1,1 +1,10 @@
+# src/anomaly.py
 
+from sklearn.ensemble import IsolationForest
+
+def detect_anomalies(df):
+    iso = IsolationForest(contamination=0.01, random_state=42)
+    df['anomaly'] = iso.fit_predict(df[['y']])
+    df['anomaly'] = df['anomaly'].map({1: 0, -1: 1})
+    
+    return df
