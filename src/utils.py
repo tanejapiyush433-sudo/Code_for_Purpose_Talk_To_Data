6124b97f3a410c60d84d42408a809e4dbe422143
tanejapiyush433-sudo <tanejapiyush433@gmail.com>
2026-04-10 23:55:21 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,13 @@
+# src/utils.py
 
+import pandas as pd
+import numpy as np
+
+def preprocess(df):
+    numeric_cols = df.select_dtypes(include=np.number).columns
+    value_col = numeric_cols[0]
+
+    df['ds'] = pd.date_range(start='2024-01-01', periods=len(df), freq='s')
+    df['y'] = df[value_col]
+
+    return df[['ds', 'y']]
