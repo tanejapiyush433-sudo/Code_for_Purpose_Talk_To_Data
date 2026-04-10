9dd8a8f1daf3cba156f5ef14510419cb970f05a0
tanejapiyush433-sudo <tanejapiyush433@gmail.com>
2026-04-11 00:09:58 +0530

    Update forecast.py

--- src/forecast.py
+++ src/forecast.py
@@ -7,7 +7,7 @@ def run_forecast(df):
         interval_width=0.95,
         changepoint_prior_scale=0.5
     )
-    model.fit(df)
+    model.fit(train_df)
 
     future = model.make_future_dataframe(periods=50, freq='s')
     forecast = model.predict(future)
