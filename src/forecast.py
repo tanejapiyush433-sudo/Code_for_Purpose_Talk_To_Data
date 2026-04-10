3d410802ba2c220c848c1e8bf8ab17d169ca23ad
tanejapiyush433-sudo <tanejapiyush433@gmail.com>
2026-04-10 23:54:36 +0530

    Update forecast.py

--- src/forecast.py
+++ src/forecast.py
@@ -1,1 +1,15 @@
+# src/forecast.py
 
+from prophet import Prophet
+
+def run_forecast(df):
+    model = Prophet(
+        interval_width=0.95,
+        changepoint_prior_scale=0.5
+    )
+    model.fit(df)
+
+    future = model.make_future_dataframe(periods=50, freq='s')
+    forecast = model.predict(future)
+
+    return forecast
