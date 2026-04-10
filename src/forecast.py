fce1da1136b426274d660f5151da39148ca3ade4
tanejapiyush433-sudo <tanejapiyush433@gmail.com>
2026-04-11 00:15:02 +0530

    Update forecast.py

--- src/forecast.py
+++ src/forecast.py
@@ -2,22 +2,15 @@
 
 from prophet import Prophet
 
-def run_forecast(df):
+def run_forecast(train_df, steps):
     model = Prophet(
         interval_width=0.95,
         changepoint_prior_scale=0.5
     )
+
     model.fit(train_df)
 
-    future = model.make_future_dataframe(periods=len(test_df), freq='s')
+    future = model.make_future_dataframe(periods=steps, freq='s')
     forecast = model.predict(future)
-    # ==============================
-    # TEST PREDICTIONS
-    # ==============================
-    forecast_test = forecast.iloc[-len(test_df):]
-
-    y_true = test_df['y'].reset_index(drop=True)
-    y_pred_model = forecast_test['yhat'].reset_index(drop=True)
-    y_pred_baseline = test_df['baseline'].reset_index(drop=True)
 
     return forecast
