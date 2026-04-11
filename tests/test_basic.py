9ba7b4f66a505d36943a071ac4692dd00127ffae
tanejapiyush433-sudo <tanejapiyush433@gmail.com>
2026-04-12 00:31:35 +0530

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,2 +1,11 @@
-def test_basic():
-    assert True
+
+import pandas as pd
+from src.utils import preprocess
+
+def test_preprocess():
+    df = pd.DataFrame({'value': [1,2,3,4,5]})
+    processed = preprocess(df)
+    
+    assert 'ds' in processed.columns
+    assert 'y' in processed.columns
+    assert len(processed) == 5
